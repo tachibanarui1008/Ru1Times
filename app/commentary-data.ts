3222bf3142993895d0a9a857804a345b15558acd
tachibanarui <1403143456@qq.com>
2026-08-22 20:54:41 +0800

    fix third commentary archive path

--- app/commentary-data.ts
+++ app/commentary-data.ts
@@ -1,6 +1,6 @@
 import { commentary20260822Contribution } from "./commentary-reports/2026-08-22-contribution";
 import { commentaryOdyssey20260822 } from "./commentary-reports/2026-08-22-odyssey";
-import { commentaryStudentLife20260822 } from "./commentary-reports/2026-08-22";
+import { commentaryStudentLife20260822 } from "./commentary-reports/2026-08-22-student-life";
 import type { CommentaryReport } from "./commentary-types";
 
 export const commentaryReports: CommentaryReport[] = [commentary20260822Contribution, commentaryOdyssey20260822, commentaryStudentLife20260822];
